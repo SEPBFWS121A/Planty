5d62ad181118360e6d709e10eb4da46072a407c2
Torben Schweren <torben@schweren.dev>
2023-04-15 18:00:42 +0200

    Merge branch feature/frontend/add-delete-tabledata into main (#21)
    * Plant adding developed
    
    * Plant deleting developed
    
    * sensor and category deleting and adding developed, submit buttons css styling edited
    
    * changes after review
    
    ---------
    
    Co-authored-by: Quadral1403 <moritz.tomeric@gmail.com>

--- frontend/src/app/components/category-management/category-management.component.ts
+++ frontend/src/app/components/category-management/category-management.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import { ApiService } from 'src/app/services/api.service';
-import { DefaultService } from 'src/assets/ts-api-client';
+import { DefaultService, PlantTypePayload } from 'src/assets/ts-api-client';
 
 @Component({
   selector: 'app-category-management',
@@ -13,13 +13,15 @@ export class CategoryManagementComponent implements OnInit {
     'name',
     'description',
     'minHumidityLevel',
+    'delete',
   ];
   data: any;
 
+  category: PlantTypePayload = {};
+
   constructor(
-    private defaultService: DefaultService
-  ) // private apiService: ApiService
-  {}
+    private defaultService: DefaultService // private apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.defaultService.plantTypeGet().subscribe((result) => {
@@ -28,5 +30,33 @@ export class CategoryManagementComponent implements OnInit {
     // this.apiService.plantTypeGet().subscribe((result) => {
     //   this.data = result;
     // });
+  }
+  async saveCategory(
+    name: string,
+    description: string,
+    minHumidityLevel: string
+  ) {
+    this.category.name = name;
+    this.category.description = description;
+    this.category.minHumidityLevel = Number(minHumidityLevel);
+    this.defaultService.plantTypePost(this.category).subscribe({
+      next(answer) {
+        location.reload();
+      },
+      error(msg) {
+        console.log('Error posting category: ', msg);
+      },
+    });
+  }
+
+  async deleteItem(data: any) {
+    await this.defaultService.plantTypePlantTypeIdDelete(data.id).subscribe({
+      next(answer) {
+        location.reload();
+      },
+      error(msg) {
+        console.log('Error posting category: ', msg);
+      },
+    });
   }
 }
