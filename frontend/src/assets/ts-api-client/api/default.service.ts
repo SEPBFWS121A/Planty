852dd5517bc235de87667fcc6e6619ba87dec54d
Kitt3120 <torben@schweren.dev>
2023-04-18 11:50:55 +0200

    Category management fix
    Fixed wrong click handling for 'Add Category' button

--- frontend/src/assets/ts-api-client/api/default.service.ts
+++ frontend/src/assets/ts-api-client/api/default.service.ts
@@ -57,7 +57,7 @@ import { Configuration } from '../configuration';
   providedIn: 'root',
 })
 export class DefaultService {
-  protected basePath = 'https://planty.schweren.dev:443';
+  protected basePath = 'http://localhost:8080';
   public defaultHeaders = new HttpHeaders();
   public configuration = new Configuration();
   public encoder: HttpParameterCodec;
