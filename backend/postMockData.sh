006ef96182663dbae87967964a16ed26c1d84714
Quadral1403 <80116260+Quadral1403@users.noreply.github.com>
2023-05-10 12:15:39 +0200

    merge Mockdatascript Bug fixed into main (#40)
    Mockdatascript Bug fixed

--- backend/postMockData.sh
+++ backend/postMockData.sh
@@ -9,8 +9,8 @@ curl -v -X POST -H "Content-Type: application/json" -d '{"name": "Sunflower", "d
 curl -v -X POST -H "Content-Type: application/json" -d '{"name": "Tulip", "description": "A family of flowers that originated from Turkey."}' http://localhost:8080/plantType
 
 # Sensor
-curl -v -X POST -H "Content-Type: application/json" -d '{"name": "Epic Sensor", "description": "The most accurate of them all. This is the main character."}' http://localhost:8080/sensor
-curl -v -X POST -H "Content-Type: application/json" -d '{"name": "Crippled Sensor", "description": "This one has cheap parts built into it."}' http://localhost:8080/sensor
+curl -v -X POST -H "Content-Type: application/json" -d '{"name": "Epic Sensor", "description": "The most accurate of them all. This is the main character.", "hardwareId": "5"}' http://localhost:8080/sensor
+curl -v -X POST -H "Content-Type: application/json" -d '{"name": "Crippled Sensor", "description": "This one has cheap parts built into it.", "hardwareId": "6"}' http://localhost:8080/sensor
 
 curl -v -X PUT -H "Content-Type: application/json" -d '{"humidityScalingFrom": 0, "humidityScalingTo": 100, "sleepTimeout": 60}' http://localhost:8080/sensor/5
 curl -v -X PUT -H "Content-Type: application/json" -d '{"humidityScalingFrom": 32, "humidityScalingTo": 65, "sleepTimeout": 60}' http://localhost:8080/sensor/6
