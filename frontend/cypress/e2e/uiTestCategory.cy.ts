1b7965d6d5b4a6318a6f37a8aab17253ffd1313a
Edward Löwen <106547857+Mijusa@users.noreply.github.com>
2023-06-10 19:39:04 +0200

    Feature/uitests (#49)
    * adding one unittest
    
    * Added UI-Test with cypress. Tests for existence checks and for functionality.
    
    * adding part of Category e2e test
    
    * UI check for category
    
    * first Category Test
    
    ---------
    
    Co-authored-by: Krzyszczyk <contact.krzyszczyk@gmail.com>

---
+++ frontend/cypress/e2e/uiTestCategory.cy.ts
@@ -0,0 +1,44 @@
+import { forEach } from 'cypress/types/lodash';
+describe('UI Test Category', () => {
+    before(() => {
+        cy.visit('http://localhost:4200/category');
+    });
+
+    describe('Check UI-Elements', () => {
+        it('Check Submit Button', () => {
+            //Check if buttons exist
+            cy.contains('button', 'Add Category').should('exist');
+        });
+
+        it('Check name field', () => {
+            //Check if field exist
+            cy.get('input[placeholder="Name"]').should('exist');
+        });
+
+        it('Check description field', () => {
+            //Check if field exist
+            cy.get('input[placeholder="Description"]').should('exist');
+        });
+
+        it('Check min humidity field', () => {
+            //Check if field exist
+            cy.get('input[placeholder="Min Humidity Level"]').should('exist');
+        });
+    });
+
+    describe('Check UI-Elements functionality', () => {
+        it('fill out form, submits and deletes the category', () => {
+            cy.get('input[placeholder="Name"]').type('Category Name');
+            cy.get('input[placeholder="Description"]').type('Category Description');
+            cy.get('input[placeholder="Min Humidity Level"]').type('10');
+
+            cy.get('button').contains('Add Category').click();
+
+        // Find the button within the last <td> element
+        cy.contains('table td', 'Category Name')
+            .siblings('td')
+            .contains('Delete')
+            .click();
+        });
+    });
+});
