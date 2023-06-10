d61eb93cc0f1214616c3dfa9f9cef3e03f056573
Mijusa <106547857+Mijusa@users.noreply.github.com>
2023-06-10 19:01:48 +0200

    UI check for category

--- frontend/cypress/e2e/uiTestCategory.cy.ts
+++ frontend/cypress/e2e/uiTestCategory.cy.ts
@@ -1,18 +1,41 @@
 import { forEach } from 'cypress/types/lodash';
-describe('Category Page button test', () => {
+describe('UI Test Category', () => {
     before(() => {
         cy.visit('http://localhost:4200/category');
     });
 
-    it('fill out form, submits and deletes the category', () => {
-        cy.get('input[placeholder="Name"]').type('Category Name');
-        cy.get('input[placeholder="Description"]').type('Category Description');
-        cy.get('input[placeholder="Min Humidity Level"]').type('10');
+    describe('Check UI-Elements', () => {
+        it('Check Submit Button', () => {
+            //Check if buttons exist
+            cy.contains('button', 'Add Category').should('exist');
+        });
 
-        cy.get('button').contains('Add Category').click();
+        it('Check name field', () => {
+            //Check if field exist
+            cy.get('input[placeholder="Name"]').should('exist');
+        });
 
-        // Find the button within the last <td> element
-        cy.get('td.mat-cell.cdk-column-delete button.mat-button').click();
-        ;
+        it('Check description field', () => {
+            //Check if field exist
+            cy.get('input[placeholder="Description"]').should('exist');
+        });
+
+        it('Check min humidity field', () => {
+            //Check if field exist
+            cy.get('input[placeholder="Min Humidity Level"]').should('exist');
+        });
+    });
+
+    describe('Check UI-Elements functionality', () => {
+        it('fill out form, submits and deletes the category', () => {
+            cy.get('input[placeholder="Name"]').type('Category Name');
+            cy.get('input[placeholder="Description"]').type('Category Description');
+            cy.get('input[placeholder="Min Humidity Level"]').type('10');
+
+            cy.get('button').contains('Add Category').click();
+
+            // Find the button within the last <td> element
+            cy.get('td.mat-cell.cdk-column-delete button.mat-button').click();
+        });
     });
 });
