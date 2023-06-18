8e0c7e45fc1b31014284f33cbcf39681493f5d34
Moritz Hegmann <hegmann.moritz@gmail.com>
2023-06-18 22:19:41 +0200

    -

--- backend/src/test/java/de/planty/unit/mappers/PlantEntityMapperTest.java
+++ backend/src/test/java/de/planty/unit/mappers/PlantEntityMapperTest.java
@@ -18,52 +18,57 @@ public class PlantEntityMapperTest {
     @Test
     public void testPlantEntityMapperPanacheEntityToGenEntity() {
         EntityPlant entityPlant = new EntityPlant();
+        entityPlant.setId(0);
         entityPlant.setName("Tannenbaum");
         entityPlant.setDescription("Ab dem 1.1. wird es Peilich...");
         entityPlant.setPlantTypeId(24);
         entityPlant.setSensorId(12);
         entityPlant.setRoomId(23);
 
-
         GenPlant genPlantPayload = PlantEntityMapper.getInstance().mapPanacheEntity(entityPlant);
 
-        assertTrue(genPlantPayload.getName().equals("Tannenbaum");
-        assertTrue(genPlantPayload.getDescription().equals("Ab dem 1.1. wird es Peilich...");
-        assertTrue(genPlantPayload.getPlantTypeId() == 24);
-        assertTrue(genPlantPayload.getSensorId() == 12);
-        assertTrue(genPlantPayload.getRoomId() == 23);
+        assertTrue(genPlantPayload.getName().equals(entityPlant.getName()));
+        assertTrue(genPlantPayload.getDescription().equals(entityPlant.getDescription()));
+        assertTrue(genPlantPayload.getPlantTypeId() == entityPlant.getPlantTypeId());
+        assertTrue(genPlantPayload.getSensorId() == entityPlant.getSensorId());
+        assertTrue(genPlantPayload.getRoomId() == entityPlant.getRoomId());
     }
 
     @Test
     public void testPlantEntityMapperGenEntityToPanacheEntity() {
-        GenPlant genPlantPayload = new GenPlant();
+        GenPlant genPlant = new GenPlant();
+        genPlant.setId(0);
+        genPlant.setName("Tannenbaum");
+        genPlant.setDescription("Ab dem 1.1. wird es Peilich...");
+        genPlant.setPlantTypeId(24);
+        genPlant.setSensorId(12);
+        genPlant.setRoomId(23);
+
+        EntityPlant entityPlant = PlantEntityMapper.getInstance().mapGenEntity(genPlant);
+
+        assertTrue(entityPlant.getId() == genPlant.getId());
+        assertTrue(entityPlant.getName().equals(genPlant.getName()));
+        assertTrue(entityPlant.getDescription().equals(genPlant.getDescription()));
+        assertTrue(entityPlant.getPlantTypeId() == genPlant.getPlantTypeId());
+        assertTrue(entityPlant.getSensorId() == genPlant.getSensorId());
+        assertTrue(entityPlant.getRoomId() == genPlant.getRoomId());
+    }
+
+    @Test
+    public void testRoomEntityMapperPayloadToPanacheEntity() {
+        GenPlantPayload genPlantPayload = new GenPlantPayload();
         genPlantPayload.setName("Tannenbaum");
         genPlantPayload.setDescription("Ab dem 1.1. wird es Peilich...");
         genPlantPayload.setPlantTypeId(24);
         genPlantPayload.setSensorId(12);
         genPlantPayload.setRoomId(23);
 
-
-        EntityPlant entityPlant = PlantEntityMapper.getInstance().mapGenEntity(genPlantPayload);
-
-        assertTrue(entityPlant.getId() == 0);
-        assertTrue(entityPlant.getName().equals("Tannenbaum");
-        assertTrue(entityPlant.getDescription().equals("Ab dem 1.1. wird es Peilich...");
-        assertTrue(entityPlant.getPlantTypeId() == 24);
-        assertTrue(entityPlant.getSensorId() == 12);
-        assertTrue(entityPlant.getRoomId() == 23);
-    }
-
-    @Test
-    public void testPlantEntityMapperPayloadToPanacheEntity() {
-        GenPlantPayload genPlantPayload = new GenPlantPayload();
-        genPlantPayload.setName("Tannenbaum");
-        genPlantPayload.setDescription("Ab dem 1.1. wird es Peilich...");
-
         EntityPlant entityPlant = PlantEntityMapper.getInstance().mapPayload(genPlantPayload);
 
-        assertTrue(entityPlant.getName().equals("Tannenbaum"));
-        assertTrue(entityPlant.getDescription().equals("Ab dem 1.1. wird es Peilich..."));
+        assertTrue(entityPlant.getName().equals(genPlantPayload.getName()));
+        assertTrue(entityPlant.getDescription().equals(genPlantPayload.getDescription()));
+        assertTrue(entityPlant.getPlantTypeId() == genPlantPayload.getPlantTypeId());
+        assertTrue(entityPlant.getSensorId() == genPlantPayload.getSensorId());
+        assertTrue(entityPlant.getRoomId() == genPlantPayload.getRoomId());
     }
-
 }
